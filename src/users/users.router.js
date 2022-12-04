c326836c1a3cd114097c368404724f402d6d6889
JoseLuisHcoder <zonajl@gmail.com>
2022-12-04 14:26:18 -0400

    rutas protegidas

--- src/users/users.router.js
+++ src/users/users.router.js
@@ -4,11 +4,11 @@ const userServices = require('./users.services')
 
 const passportJWT = require('../middleware/auth.middleware')
 
-router.get("/",passportJWT.authenticate('jwt', {session:false}) , userServices.getAllUsers) //? /api/v1/users
+router.get("/" , userServices.getAllUsers) //? /api/v1/users
 router.post("/", userServices.postUser) //? /api/v1/users
 
 router.get("/:id", userServices.getUserById) //? /api/v1/users/:id
-router.patch('/:id', userServices.patchUser) //? /api/v1/users/:id
-router.delete('/:id', userServices.deleteUser) //? /api/v1/users/:id
+router.patch('/:id', passportJWT.authenticate('jwt', {session:false}), userServices.patchUser) //? /api/v1/users/:id
+router.delete('/:id', passportJWT.authenticate('jwt', {session:false}), userServices.deleteUser) //? /api/v1/users/:id
 
 module.exports = router
